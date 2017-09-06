e0789c264525adbf8a227246ff038513844f9acf
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-06 14:12:19 +0200

    Remove allowed_hosts for vanilla settings

--- login/settings.py
+++ login/settings.py
@@ -26,8 +26,6 @@ SECRET_KEY = '^3i755m#nrt5hqhn&722t@@9%_4(*&u3=-vy(i76!03*dg*)ev'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    'ec2-18-194-42-178.eu-central-1.compute.amazonaws.com'
 ]
 
 
