e8fb54834ee6902b5576015a32077a57f6a3e869
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-19 14:24:21 +0200

    Update serializers add serialization of is_staff etc.

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,6 @@ from users.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        fields = ('url', 'id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+        read_only_fields = ('url', 'id', 'is_active', 'is_staff', 'is_superuser')
+
